aeca77852b6d265f9561487417635b677d6a674a
Yong Lu <yongg2719@gmail.com>
2026-06-28 20:41:30 +0000

    Added color properties to street property class

--- models/tiles/properties/street_property.py
+++ models/tiles/properties/street_property.py
@@ -5,9 +5,10 @@ from models.tiles.property import Property
 class StreetProperty(Property):
     """Inherits from Property class"""
 
-    def __init__(self, name, pos, price, rent_table):
+    def __init__(self, name, pos, price, rent_table, color):
         super().__init__(name, pos, price)
         self.rent_table = rent_table
+        self.color = color
         self.houses = 0
 
     def get_rent(self,game,player):
